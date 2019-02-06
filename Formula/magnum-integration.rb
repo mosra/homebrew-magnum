ded8123ac7b5a4467f1f84fa5898a2a6ac1dfb2a
Vladimír Vondruš <mosra@centrum.cz>
2019-02-06 19:50:27 +0100

    Update to 2019.01.

--- Formula/magnum-integration.rb
+++ Formula/magnum-integration.rb
@@ -1,8 +1,9 @@
 class MagnumIntegration < Formula
   desc "Integration libraries for the Magnum C++11/C++14 graphics engine"
   homepage "https://magnum.graphics"
-  url "https://github.com/mosra/magnum-integration/archive/v2018.10.tar.gz"
-  sha256 "f999800faf5fe0f24f5de7ad42fc11f3371c377e95a9c7b667c45bf264b3ceaf"
+  url "https://github.com/mosra/magnum-integration/archive/v2019.01.tar.gz"
+  # wget https://github.com/mosra/magnum-integration/archive/v2019.01.tar.gz -O - | sha256sum
+  sha256 "a1de0eeb4b9ff58a1d4320e5632a4c84f9e54bc554c22aba334cafa1e7c7e868"
   head "git://github.com/mosra/magnum-integration.git"
 
   depends_on "cmake"
@@ -13,7 +14,7 @@ class MagnumIntegration < Formula
   def install
     system "mkdir build"
     cd "build" do
-      system "cmake", "-DCMAKE_BUILD_TYPE=Release", "-DCMAKE_INSTALL_PREFIX=#{prefix}", "-DWITH_BULLET=ON", "-DWITH_DART=OFF", "-DWITH_GLM=ON", ".."
+      system "cmake", "-DCMAKE_BUILD_TYPE=Release", "-DCMAKE_INSTALL_PREFIX=#{prefix}", "-DWITH_BULLET=ON", "-DWITH_DART=OFF", "-DWITH_GLM=ON", "-DWITH_IMGUI=OFF", ".."
       system "cmake", "--build", "."
       system "cmake", "--build", ".", "--target", "install"
     end
