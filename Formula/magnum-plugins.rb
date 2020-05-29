22217f976ddc6445af525e370f19c10e9c96be46
Vladimír Vondruš <mosra@centrum.cz>
2020-05-29 18:34:14 +0200

    Use bundled Basis and meshoptimizer in magnum-plugins.

--- Formula/magnum-plugins.rb
+++ Formula/magnum-plugins.rb
@@ -17,9 +17,26 @@ class MagnumPlugins < Formula
   depends_on "jpeg"
 
   def install
+    # Bundle Basis Universal, a commit that's before the UASTC support (which
+    # is not implemented yet). The repo has massive useless files in its
+    # history, so we're downloading just a snapshot instead of a git clone.
+    # Also, WHY THE FUCK curl needs -L and -o?! why can't it just work?!
+    system "curl", "-L", "https://github.com/BinomialLLC/basis_universal/archive/2f43afcc97d0a5dafdb73b4e24e123cf9687a418.tar.gz", "-o", "src/external/basis-universal.tar.gz"
+    cd "src/external" do
+      system "mkdir", "basis-universal"
+      system "tar", "xzvf", "basis-universal.tar.gz", "-C", "basis-universal", "--strip-components=1"
+    end
+
+    # Bundle meshoptimizer 0.14
+    system "curl", "-L", "https://github.com/zeux/meshoptimizer/archive/v0.14.tar.gz", "-o", "src/external/meshoptimizer.tar.gz"
+    cd "src/external" do
+      system "mkdir", "meshoptimizer"
+      system "tar", "xzvf", "meshoptimizer.tar.gz", "-C", "meshoptimizer", "--strip-components=1"
+    end
+
     system "mkdir build"
     cd "build" do
-      system "cmake", "-DCMAKE_BUILD_TYPE=Release", "-DCMAKE_INSTALL_PREFIX=#{prefix}", "-DWITH_ASSIMPIMPORTER=ON", "-DWITH_BASISIMAGECONVERTER=OFF", "-DWITH_BASISIMPORTER=OFF", "-DWITH_DDSIMPORTER=ON", "-DWITH_DEVILIMAGEIMPORTER=ON", "-DWITH_DRFLACAUDIOIMPORTER=ON", "-DWITH_DRMP3AUDIOIMPORTER=ON", "-DWITH_DRWAVAUDIOIMPORTER=ON", "-DWITH_FAAD2AUDIOIMPORTER=ON", "-DWITH_FREETYPEFONT=ON", "-DWITH_HARFBUZZFONT=ON", "-DWITH_JPEGIMAGECONVERTER=ON", "-DWITH_JPEGIMPORTER=ON", "-DWITH_MESHOPTIMIZERSCENECONVERTER=OFF", "-DWITH_MINIEXRIMAGECONVERTER=ON", "-DWITH_OPENGEXIMPORTER=ON", "-DWITH_PNGIMAGECONVERTER=ON", "-DWITH_PNGIMPORTER=ON", "-DWITH_PRIMITIVEIMPORTER=ON", "-DWITH_STANFORDIMPORTER=ON", "-DWITH_STANFORDSCENECONVERTER=ON", "-DWITH_STBIMAGECONVERTER=ON", "-DWITH_STBIMAGEIMPORTER=ON", "-DWITH_STBTRUETYPEFONT=ON", "-DWITH_STBVORBISAUDIOIMPORTER=ON", "-DWITH_STLIMPORTER=ON", "-DWITH_TINYGLTFIMPORTER=ON", ".."
+      system "cmake", "-DCMAKE_BUILD_TYPE=Release", "-DCMAKE_INSTALL_PREFIX=#{prefix}", "-DWITH_ASSIMPIMPORTER=ON", "-DWITH_BASISIMAGECONVERTER=ON", "-DWITH_BASISIMPORTER=ON", "-DWITH_DDSIMPORTER=ON", "-DWITH_DEVILIMAGEIMPORTER=ON", "-DWITH_DRFLACAUDIOIMPORTER=ON", "-DWITH_DRMP3AUDIOIMPORTER=ON", "-DWITH_DRWAVAUDIOIMPORTER=ON", "-DWITH_FAAD2AUDIOIMPORTER=ON", "-DWITH_FREETYPEFONT=ON", "-DWITH_HARFBUZZFONT=ON", "-DWITH_JPEGIMAGECONVERTER=ON", "-DWITH_JPEGIMPORTER=ON", "-DWITH_MESHOPTIMIZERSCENECONVERTER=ON", "-DWITH_MINIEXRIMAGECONVERTER=ON", "-DWITH_OPENGEXIMPORTER=ON", "-DWITH_PNGIMAGECONVERTER=ON", "-DWITH_PNGIMPORTER=ON", "-DWITH_PRIMITIVEIMPORTER=ON", "-DWITH_STANFORDIMPORTER=ON", "-DWITH_STANFORDSCENECONVERTER=ON", "-DWITH_STBIMAGECONVERTER=ON", "-DWITH_STBIMAGEIMPORTER=ON", "-DWITH_STBTRUETYPEFONT=ON", "-DWITH_STBVORBISAUDIOIMPORTER=ON", "-DWITH_STLIMPORTER=ON", "-DWITH_TINYGLTFIMPORTER=ON", ".."
       system "cmake", "--build", "."
       system "cmake", "--build", ".", "--target", "install"
     end
